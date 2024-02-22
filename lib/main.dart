c5dcb4ff561a837ea2bb1381f4cb1682d570ffa6
Joss Paddock <josspaddock@Josss-MacBook-Pro.local>
2024-02-21 17:59:42 -0800

    sorting out widget options

--- lib/main.dart
+++ lib/main.dart
@@ -26,9 +26,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool signedIn = false;
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _signedInWidgetOptions = <Widget>[
+    Text(
+      'Index 0: Replace this text widget with the google map widget',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Replace this text widget with the My Interests widget',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Replace this text widget with the Account widget',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Replace this text widget with the Messages widget',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: Replace this text widget with the Sign Out widget',
+      style: optionStyle,
+    ),
+  ];
   static const List<Widget> _signedOutWidgetOptions = <Widget>[
     Text(
       'Index 0: Replace this text widget with the google map widget',
@@ -38,10 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
       'Index 1: Replace this text widget with the sign-in page widget',
       style: optionStyle,
     ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
+
   ];
 
   void _onItemTapped(int index) {
@@ -55,13 +75,74 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
       body: Center(
-        child: _signedOutWidgetOptions[_selectedIndex],
+        child: signedIn ? _signedInWidgetOptions[_selectedIndex] : _signedOutWidgetOptions[_selectedIndex],
       ),
       drawer: Drawer(
         // Add a ListView to the drawer. This ensures the user can scroll
         // through the options in the drawer if there isn't enough vertical
         // space to fit everything.
-        child: ListView(
+        child: signedIn ? ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Map'),
+              selected: _selectedIndex == 0,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(0);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('My Interests'),
+              selected: _selectedIndex == 1,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(1);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Account'),
+              selected: _selectedIndex == 2,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(2);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Messages'),
+              selected: _selectedIndex == 3,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(3);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Sign Out'),
+              selected: _selectedIndex == 4,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(4);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ) : ListView(
           // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
           children: [
