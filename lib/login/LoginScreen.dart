c0a32d96cd6e35f4297c84119d2a4c3d83abeb63
UltimateOgee <shubhankar.singh@outlook.com>
2024-02-28 16:45:03 -0800

    added a working sign in widget with sign in, create account, and forgot password with animations included, also have mix and match approach to passing around states between a parent and child widget

---
+++ lib/login/LoginScreen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+
+const users =  {
+  'test@test.com': 'password',
+};
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({
+    super.key,
+    this.signedIn = false,
+    required this.onSignInChanged,
+    required this.onSelectedIndexChanged,
+  });
+
+  final bool signedIn;
+  final ValueChanged<bool> onSignInChanged;
+  final ValueChanged<int> onSelectedIndexChanged;
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  Duration get loginTime => const Duration(milliseconds: 2250);
+
+  Future<String?> _authUser(LoginData data) {
+    debugPrint('Name: ${data.name}, Password: ${data.password}');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(data.name)) {
+        return 'User not exists';
+      }
+      if (users[data.name] != data.password) {
+        return 'Password does not match';
+      }
+      return null;
+    });
+  }
+
+  Future<String?> _signupUser(SignupData data) {
+    debugPrint('Signup Name: ${data.name}, Password: ${data.password}');
+    return Future.delayed(loginTime).then((_) {
+      return null;
+    });
+  }
+
+  Future<String?> _recoverPassword(String name) {
+    debugPrint('Name: $name');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(name)) {
+        return 'User not exists';
+      }
+      return null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogin(
+      title: 'ECORP',
+      //if you want a log above the login widget, add the path to a png, eg below:
+      //logo: const AssetImage('assets/images/ecorp-lightblue.png'),
+      onLogin: _authUser,
+      onSignup: _signupUser,
+      onSubmitAnimationCompleted: () {
+        debugPrint("onSubmitAnimationCompleted: User logged in");
+        widget.onSignInChanged(true);
+        widget.onSelectedIndexChanged(0);
+      },
+      onRecoverPassword: _recoverPassword,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'login/LoginScreen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -26,8 +27,41 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool signedIn = false;
+  
+  bool _signedIn = false;
   int _selectedIndex = 0;
+
+  late List<Widget> _signedOutWidgetOptions;
+
+  @override
+  void initState() {
+    _signedOutWidgetOptions = <Widget>[
+    Text(
+      'Index 0: Replace this text widget with the google map widget',
+      style: optionStyle,
+    ),
+    //sign in widget AKA the LoginScreen will go here
+    LoginScreen(
+      signedIn : _signedIn,
+      onSignInChanged : _handleSignInChanged,
+      onSelectedIndexChanged: _onItemTapped,
+    ),
+  ];
+    super.initState();
+  }
+
+  void _handleSignInChanged(bool newValue) {
+    setState(() {
+      _signedIn = newValue;
+    });
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _signedInWidgetOptions = <Widget>[
@@ -52,60 +86,19 @@ class _MyHomePageState extends State<MyHomePage> {
       style: optionStyle,
     ),
   ];
-  static List<Widget> _signedOutWidgetOptions = <Widget>[
-    Text(
-      'Index 0: Replace this text widget with the google map widget',
-      style: optionStyle,
-    ),
-    Container(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          constraints: BoxConstraints(minWidth: 300, maxWidth: 800),
-          child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          constraints: BoxConstraints(minWidth: 200, maxWidth: 800),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Email',
-            ),
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          constraints: BoxConstraints(minWidth: 50, maxWidth: 800),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Password',
-            ),
-          ),
-        ),
-      ],
-    ),
-        ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
       body: Center(
-        child: signedIn ? _signedInWidgetOptions[_selectedIndex] : _signedOutWidgetOptions[_selectedIndex],
+        child: _signedIn ? _signedInWidgetOptions[_selectedIndex] : _signedOutWidgetOptions[_selectedIndex],
       ),
       drawer: Drawer(
         // Add a ListView to the drawer. This ensures the user can scroll
         // through the options in the drawer if there isn't enough vertical
         // space to fit everything.
-        child: signedIn ? ListView(
+        child: _signedIn ? ListView(
           // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
           children: [
