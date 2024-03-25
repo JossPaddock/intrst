06dd03cb7bd075458ae5aff0248c0db4394e7668
josspaddock <josspaddock@hotmail.com>
2024-03-25 16:29:20 -0700

    Removes document update capability from ButtonWidget
    adds API key to README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # intrst
 This is an open source project to connect people with similar interests.
+
+### API key for the google map 
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAmvGk8E1151aZnnwvDnW-FQvMiha-HEJU"></script>
--- lib/widgets/ButtonWidget.dart
+++ lib/widgets/ButtonWidget.dart
@@ -4,30 +4,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class ButtonWidget extends StatelessWidget{
- String user_uid = '';
-
-  void initState() {
-    FirebaseAuth.instance
-        .authStateChanges()
-        .listen((User? user) {
-      if (user != null) {
-        print(user.uid);
-        user_uid = user.uid;
-      }
-    });
-  }
   void addUserToFirestore(){
-    CollectionReference users = FirebaseFirestore.instance.collection('users');
-    Map<String, dynamic> userData = {
-      'user_uid': user_uid,
-      'first_name': 'John',
-      'last_name': 'Doe',
-      'interests': [],
-      'location': GeoPoint(0,0),
-    };
-    users.add(userData)
-        .then((value) => print("User added to Firestore"))
-        .catchError((error) => print("Failed to add user: $error"));
+   print("user clicked 'click me' button");
   }
   Widget build(BuildContext context){
     return TextButton(onPressed: () { addUserToFirestore(); }, child: Text('Click Me'),);
