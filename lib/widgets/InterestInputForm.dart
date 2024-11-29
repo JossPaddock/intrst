666e4c4f186604dd8be562f62fa4dc44764d8a57
josspaddock <josspaddock@hotmail.com>
2024-11-29 13:33:02 -0800

    see how ui changes for interest input form work on mobile

--- lib/widgets/InterestInputForm.dart
+++ lib/widgets/InterestInputForm.dart
@@ -48,100 +48,111 @@ class InterestInputFormState extends State<InterestInputForm> {
                   context: context,
                   barrierDismissible: false,
                   builder: (BuildContext context) => AlertDialog(
-                    content: Form(
-                      key: _formKey,
-                      child: Card(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            TextFormField(
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Please enter some text';
-                                }
-                                interest.name = value;
-                                return null;
-                              },
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                hintText: 'Add a new interest.',
-                              ),
-                            ),
-                            TextFormField(
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Please enter some text';
-                                }
-                                interest.description = value;
-                                return null;
-                              },
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                hintText:
-                                    'Enter a description of the interest.',
-                              ),
-                            ),
-                            TextFormField(
-                              validator: (value) {
-                                if (value != null && value.isNotEmpty) {
-                                  if (!hasValidUrl(value)) {
-                                    return 'Please make sure this is a valid link';
+                    content: ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxWidth: 500,
+                        maxHeight: 250,
+                      ),
+                      child: Form(
+                        key: _formKey,
+                        child: Card(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              TextFormField(
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter some text';
                                   }
-                                }
-                                interest.link = value;
-                                return null;
-                              },
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                hintText:
-                                    '(Optional) Enter a link for the interest.',
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 20),
-                              child: ElevatedButton(
-                                onPressed: () async {
-                                  // Validate returns true if the form is valid, or false otherwise.
-                                  if (_formKey.currentState!.validate()) {
-                                    // If the form is valid, display a snackbar for confirmation
-                                    // Also make call to add interest for the given user uid (logged in user)
-                                    CollectionReference users =
-                                        FirebaseFirestore.instance
-                                            .collection('users');
-                                    fu.addInterestForUser(
-                                        users, interest, user.currentUid);
-
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      const SnackBar(
-                                          content: Text('Adding new interest')),
-                                    );
-                                    // todo: future state should not delay this synchronous process but instead manage a local set of interests against the ones stored in the database
-                                    await Future.delayed(
-                                        Duration(milliseconds: 500));
-                                    UserModel userModel =
-                                        Provider.of<UserModel>(context,
-                                            listen: false);
-                                    userModel.notify();
-                                  }
+                                  interest.name = value;
+                                  return null;
                                 },
-                                child: const Text('Submit'),
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'Add a new interest.',
+                                ),
                               ),
-                            ),
-                          ],
+                              TextFormField(
+                                maxLines: 2,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter some text';
+                                  }
+                                  interest.description = value;
+                                  return null;
+                                },
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText:
+                                      'Enter a description of the interest.',
+                                ),
+                              ),
+                              TextFormField(
+                                validator: (value) {
+                                  if (value != null && value.isNotEmpty) {
+                                    if (!hasValidUrl(value)) {
+                                      return 'Please make sure this is a valid link';
+                                    }
+                                  }
+                                  interest.link = value;
+                                  return null;
+                                },
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText:
+                                      '(Optional) Enter a link for the interest.',
+                                ),
+                              ),
+                              Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 20),
+                                  child: Row(
+                                    children: [
+                                      ElevatedButton(
+                                        onPressed: () async {
+                                          // Validate returns true if the form is valid, or false otherwise.
+                                          if (_formKey.currentState!
+                                              .validate()) {
+                                            // If the form is valid, display a snackbar for confirmation
+                                            // Also make call to add interest for the given user uid (logged in user)
+                                            CollectionReference users =
+                                                FirebaseFirestore.instance
+                                                    .collection('users');
+                                            fu.addInterestForUser(users,
+                                                interest, user.currentUid);
+
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(
+                                              const SnackBar(
+                                                  content: Text(
+                                                      'Adding new interest')),
+                                            );
+                                            // todo: future state should not delay this synchronous process but instead manage a local set of interests against the ones stored in the database
+                                            await Future.delayed(
+                                                Duration(milliseconds: 500));
+                                            UserModel userModel =
+                                                Provider.of<UserModel>(context,
+                                                    listen: false);
+                                            userModel.notify();
+                                            Navigator.pop(context, 'submit');
+                                          }
+                                        },
+                                        child: const Text('Submit'),
+                                      ),
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          Navigator.pop(context, 'cancel');
+                                        },
+                                        child: const Text('Cancel'),
+                                      ),
+                                    ],
+                                  )),
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                    actions: <Widget>[
-                      Center(
-                        child: Row(children: <Widget>[
-                          IconButton(icon: Icon(Icons.save), onPressed: () {}),
-                          IconButton(
-                              icon: Icon(Icons.cancel), onPressed: () {
-                                Navigator.pop(context, 'cancel');
-                          }),
-                        ]),
-                      ),
-                    ],
+                    actions: <Widget>[],
                   ),
                 );
               }),
